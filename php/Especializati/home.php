bf5a763a516d44c77a3f306a223934a065ebd655
Anderson Coimbra <andersonmegax@hotmail.com>
2016-10-16 20:03:47 -0200

    Codigos usados na Aula de  Relacionamento Laravel Do EspecializaTi

---
+++ php/Especializati/home.php
@@ -0,0 +1,13 @@
+////////////////////////
+//Comando  para  laravel
+////////////////////////
+
+composer create-project --prefer-dist laravel/laravel nomeDoProjeto
+
+chmod -R 755 storange/
+
+php artisan key:generate
+
+///////////////////
+//Codigos  fontes
+//////////////////
