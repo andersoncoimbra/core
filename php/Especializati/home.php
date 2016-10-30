6d19c9a1640e6d18607bc4690dc1f2228576ff66
Anderson Coimbra <andersonmegax@hotmail.com>
2016-10-29 23:43:27 -0300

    Atualição do dia 23/10/2016

--- php/Especializati/home.php
+++ php/Especializati/home.php
@@ -1,13 +1,48 @@
+<?php
 ////////////////////////
 //Comando  para  laravel
 ////////////////////////
 
-composer create-project --prefer-dist laravel/laravel nomeDoProjeto
+// composer create-project --prefer-dist laravel/laravel nomeDoProjeto
 
-chmod -R 755 storange/
+// chmod -R 755 storange/
 
-php artisan key:generate
+// php artisan key:generate
 
-///////////////////
+/////////////////
 //Codigos  fontes
 //////////////////
+
+//Chave esstrangeira para inclusão na migration
+
+$table->foreign('colunaQueRecebeReferencia')->referencs('colunaDaTabelaReferencia')->on('tabelaDaReferencia')->onDelete('cascade')
+
+//modo de como alterar uma tabela pela migration
+
+$table->decimal(2,7)->change();
+
+jete de chamna a ateção so para tecclac
+
+// Atributos para desabiliar o Timestamps da model
+
+public $timestamps = false;
+
+//Metodo para atualiza coluna de uma tabela pela Model
+
+$instaciadaModel->update([
+	"colunaDaTabela" => "Valor a se atualizado"
+	])
+
+//Retorno Funçoes de relacionamento
+///////////////////////////////////
+
+	//Relacionamento do tipo  "Tem Um"
+return $this->hasOne(App\Class::class, 'colunaNaTabelaRelacionada', "colunadoIDDessaModel")
+
+	//Relacionamento do tipo  "Pertence a Um"
+return $this->belongTo(App\Class::class,"colunadoIDDessaModel")
+
+	//Relacionamento do tipo  "Tem muntos"
+return $this->hasMany(App\Class::class,"colunadoIDDessaModel")
+
+ ?>
