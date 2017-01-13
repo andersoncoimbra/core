bbe6e874e36afc34f5d562f348dfd2ce14cade34
Anderson Coimbra <andersonmegax@hotmail.com>
2017-01-13 16:08:58 -0300

    Atualização do Not Kellon

--- php/Especializati/home.php
+++ php/Especializati/home.php
@@ -21,7 +21,9 @@ $table->foreign('colunaQueRecebeReferencia')->referencs('colunaDaTabelaReferenci
 
 $table->decimal(2,7)->change();
 
-jete de chamna a ateção so para tecclac
+//metodo para encripitaçãpo
+
+bcrypy('dados a serem encripitados')
 
 // Atributos para desabiliar o Timestamps da model
 
@@ -33,6 +35,15 @@ $instaciadaModel->update([
 	"colunaDaTabela" => "Valor a se atualizado"
 	])
 
+//Metodos de associação e desassociação e sicronização] de relacionamentos "Muitos para Muitos"
+///////////////////////////////////////////////////////////////////////////////////////////////
+		//Associação
+$Estancia_da_Model->metado_que_relaciona()->attach('id ou arrays com valores do ids')
+		//Desassociação
+$Estancia_da_Model->metado_que_relaciona()->detach('id ou arrays com valores do ids')
+		//Sicronismo
+$Estancia_da_Model->metado_que_relaciona()->sync('id ou arrays com valores do ids')
+
 //Retorno Funçoes de relacionamento
 ///////////////////////////////////
 
@@ -43,6 +54,15 @@ return $this->hasOne(App\Class::class, 'colunaNaTabelaRelacionada', "colunadoIDD
 return $this->belongTo(App\Class::class,"colunadoIDDessaModel")
 
 	//Relacionamento do tipo  "Tem muntos"
-return $this->hasMany(App\Class::class,"colunadoIDDessaModel")
+return $this->hasMany(App\Class::class,"colunadoIdDessaModel")
 
+	//Relacionamento do tipo  "Tem muntos"
+return $this->belongToMany(App\Class::class,"colunadoIdDessaModel", "id_na_coluna_pivot_de_model", "id_na_coluna_da_model_a_ser_relacionada")
+
+
+ // Redirect com Parametros - muito util
+ ////////////////////////////////////////
+ $param = ['id'=>$id,'evg'=>$evg];
+ return redirect()->route('get.extras',$param);
  ?>
+
